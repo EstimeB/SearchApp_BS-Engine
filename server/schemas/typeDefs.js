93d127faa7fe79aeec748e272f571dbba373d804
brith <bgc@briths-MacBook-Air.local>
2022-05-25 14:53:00 -0700

    Deleted routes folder, added schemas folder with files, filled out typeDefs and index js files.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// 
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        books(username: String): [Book]
+        book(bookId: ID!): Book
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(authors: String!, description: String!, bookId: String!, image: String!, title: String!, link: String!): User
+        removeBook(thoughtId: ID!): User
+    }
+
+`
